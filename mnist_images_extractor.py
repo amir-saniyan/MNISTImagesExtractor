e4868971d4a5d17a8cf464914bcf846af925f5a1
Amir Saniyan <amir_saniyan@yahoo.com>
2018-04-20 18:03:24 +0430

    Improved code performance.

--- mnist_images_extractor.py
+++ mnist_images_extractor.py
@@ -1,55 +1,52 @@
 # -*- coding: utf-8 -*-
 
+# MNIST Images Extractor
+# Python code for extracting MNIST dataset images.
+
+# MNIST Dataset:
+# http://yann.lecun.com/exdb/mnist/
+
+# Repository:
+# https://github.com/amir-saniyan/MNISTImagesExtractor
+
 import os
 import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 import imageio
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=False)
 
 # Train set.
 for i in range(mnist.train.num_examples):
-    img = mnist.train.images[i].reshape(28, 28)
+    image = mnist.train.images[i].reshape(28, 28)
     label = mnist.train.labels[i]
-    for j in range(10):
-        if label[j] == 1:
-            cls = j
-            break
-    directory_name = './images/train/' + str(cls)
+    directory_name = './images/train/' + str(label)
     if not os.path.exists(directory_name):
         os.makedirs(directory_name)
     file_name = directory_name + '/' + str(i) + '.png'
     print('Saving', file_name, '...')
-    imageio.imwrite(file_name, (img * 255).astype(np.uint8))
+    imageio.imwrite(file_name, (image * 255).astype(np.uint8))
 
 # Test set.
 for i in range(mnist.test.num_examples):
-    img = mnist.test.images[i].reshape(28, 28)
+    image = mnist.test.images[i].reshape(28, 28)
     label = mnist.test.labels[i]
-    for j in range(10):
-        if label[j] == 1:
-            cls = j
-            break
-    directory_name = './images/test/' + str(cls)
+    directory_name = './images/test/' + str(label)
     if not os.path.exists(directory_name):
         os.makedirs(directory_name)
     file_name = directory_name + '/' + str(i) + '.png'
     print('Saving', file_name, '...')
-    imageio.imwrite(file_name, (img * 255).astype(np.uint8))
+    imageio.imwrite(file_name, (image * 255).astype(np.uint8))
 
 # Validation set.
 for i in range(mnist.validation.num_examples):
-    img = mnist.validation.images[i].reshape(28, 28)
+    image = mnist.validation.images[i].reshape(28, 28)
     label = mnist.validation.labels[i]
-    for j in range(10):
-        if label[j] == 1:
-            cls = j
-            break
-    directory_name = './images/validation/' + str(cls)
+    directory_name = './images/validation/' + str(label)
     if not os.path.exists(directory_name):
         os.makedirs(directory_name)
     file_name = directory_name + '/' + str(i) + '.png'
     print('Saving', file_name, '...')
-    imageio.imwrite(file_name, (img * 255).astype(np.uint8))
+    imageio.imwrite(file_name, (image * 255).astype(np.uint8))
 
 print('OK')
